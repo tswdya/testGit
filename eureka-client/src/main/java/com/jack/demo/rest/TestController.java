6ac8f3abeed6340f53e87f6313992de95071615f
AUTHOR <tswdya@126.com>
2018-06-26 13:57:37 +0800

    my test project

---
+++ eureka-client/src/main/java/com/jack/demo/rest/TestController.java
@@ -0,0 +1,24 @@
+package com.jack.demo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+	
+	@Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/test")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+
+}
